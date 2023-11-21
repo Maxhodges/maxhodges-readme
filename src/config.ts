6553614cd571393a01e8e155c8684e283f7c0721
MaxHodges <max@whiterabbitjapan.com>
2023-11-21 14:32:17 +0900

    update copy

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Max Hodges | Personal Page" ';
+export const SITE_TITLE = 'Max Hodges | Personal Page';
 export const SITE_DESCRIPTION = 'Entrepreneurial Maker & Creative Technologist".';
